c86582d0864e6380159a423c250d1c18da362c9a
NRM10101 <126091408+NRM10101@users.noreply.github.com>
2023-05-18 14:40:05 +0530

    Lab 10-first Commit

---
+++ Lab10/main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include<list>
+using namespace std;
+
+struct Node{
+    // A node will 2 entities
+    //1. data type int called label
+    int label;
+    //2. a int type list called neighbours
+    list<int> neighbours;
+
+};
+struct Graph{
+    //graph will have an array of type "node" with length specified by n
+    int n=8;
+    Node * nodes = new Node[n];
+
+    void intializenodes(){
+        //iterate through the nodes and assign labels
+        for(int i=0;i<n;i++){
+            nodes[i].label=i+1;
+        }
+    }
+    void addedge(int u, int v){  //for undirected graphs
+        //select node u and push v into u's neighbour
+        //select node v and push u into v's neighbour
+        for(int i=0;i<n;i++){
+            Node &node=nodes[i];
+            if(node.label==u){
+                node.neighbours.push_back(v);
+            }
+            if(node.label==v){
+                node.neighbours.push_back(u);
+            }
+//            if(nodes[i].label==u){
+//                nodes[i].neighbours.push_back(v);
+//            }
+//            if(nodes[i].label==v){
+//                nodes[i].neighbours.push_back(u);
+//            }
+        }
+    }
+//    void addedge(int from, int to){    //for directed graph
+//        //select node from and push to into from 's neighbour
+//        for(int i=0;i<n;i++){
+//            Node &node=nodes[i];
+//            if(node.label==from){
+//                node.neighbours.push_back(to);
+//            }
+//        }
+//    }
+    void print(){
+        //lets iterate through each node and print its neighbours
+        for(int i=0;i<n;i++) {
+            Node node=nodes[i];
+            cout<<node.label<<"->";
+            if(node.neighbours.empty()){
+                cout<<"empty"<<endl;
+            }
+            for (int neighbour: node.neighbours){
+                cout<<neighbour<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main() {
+    Graph * g = new Graph;
+    g->intializenodes();
+    //add edges for the graphs here.
+    g->addedge(1,2);
+    g->addedge(1,3);
+    g->addedge(1,4);
+    g->addedge(1,5);
+
+    g->addedge(2,3);
+    g->addedge(2,6);
+
+    g->addedge(4,6);
+    g->addedge(4,7);
+    g->addedge(4,8);
+
+    g->addedge(5,6);
+    g->addedge(5,7);
+    g->addedge(5,8);
+
+//    for(int i=0;i<8;i++){
+//        cout<<g->nodes[i].label<<"-"<<g->nodes[i].neighbours.empty()<<"+";
+//    }
+//    cout<<endl;
+
+    //print the graph adjaceny list
+    g->print();
+}
